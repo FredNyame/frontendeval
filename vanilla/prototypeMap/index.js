88c1d48ff194c592511eed435181feff10fe293a
FredNyame <fnyame63@gmail.com>
2023-12-11 03:11:38 -0500

    Complete the functionality

--- vanilla/prototypeMap/index.js
+++ vanilla/prototypeMap/index.js
@@ -1,12 +1,17 @@
 // Goal is to implement the functionality of the Array.prototype.map
 
-Array.prototype.customMap = (callback) => {
+Array.prototype.customMap = function (callback) {
   const currentValues = this;
   const returnValues = [];
 
   for (let index = 0; index < currentValues.length; index++) {
-    callback(currentValues[i], index);
+    callback(currentValues[index], index);
   }
 
   return returnValues;
 };
+
+const values = [1, 2, 3, 4];
+const newValues = values.customMap((data, index) => {
+  console.log(`Index: ${index} data:${data}`);
+});
